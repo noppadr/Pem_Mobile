365bf512748d79f4b4018e5e07f2b0f718b42e01
Nova Diana <nopadr12@gmail.com>
2025-10-03 17:48:08 +0700

    feat: improve BMI calculation logic

--- jobsheet_3/lib/calculatorBMI.dart
+++ jobsheet_3/lib/calculatorBMI.dart
@@ -1,23 +1,32 @@
 void main() {
   List<Map<String, dynamic>> riwayat = [];
 
-  // simulasi beberapa input
+  // Simulasi beberapa input
   hitungBMI(170, 65, riwayat);
   hitungBMI(165, 75, riwayat);
   hitungBMI(180, 85, riwayat);
-
-  // tampilkan riwayat menggunakan perulangan
-  tampilkanRiwayat(riwayat);
 }
+
 void hitungBMI(
   double tinggiCm,
   double beratKg,
   List<Map<String, dynamic>> riwayat,
 ) {
-  // konversi tinggi dari cm ke m
+  // Konversi tinggi dari cm ke m
   double tinggiM = tinggiCm / 100;
 
-  // hitung BMI
+  // Hitung BMI
   double bmi = beratKg / (tinggiM * tinggiM);
 
+  // Tentukan kategori menggunakan percabangan
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
+  }
 }
