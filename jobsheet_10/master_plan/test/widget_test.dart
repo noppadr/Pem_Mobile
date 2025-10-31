5b8101541d1d3dbecbf19e723efd12a5516ef270
Nova Diana <nopadr12@gmail.com>
2025-11-01 00:41:18 +0700

    Prak 1 - Langkah 1: Buat Project Baru

--- jobsheet_10/master_plan/test/widget_test.dart
+++ jobsheet_10/master_plan/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:master_plan/main.dart';
+import 'package:master_plan/views/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
