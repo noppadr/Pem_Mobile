cac763695d728390b74c2d99bb0559a9232f988f
Nova Diana <nopadr12@gmail.com>
2025-11-01 01:39:06 +0700

    Prak 1 - Langkah 11: tambah scroll listener

--- jobsheet_10/master_plan/lib/views/plan_screen.dart
+++ jobsheet_10/master_plan/lib/views/plan_screen.dart
@@ -11,7 +11,16 @@ class PlanScreen extends StatefulWidget {
 class _PlanScreenState extends State<PlanScreen> {
   Plan plan = const Plan();
   late ScrollController scrollController;
-  
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController()
+      ..addListener(() {
+        FocusScope.of(context).requestFocus(FocusNode());
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +29,7 @@ class _PlanScreenState extends State<PlanScreen> {
       floatingActionButton: _buildAddTaskButton(),
     );
   }
-  
+
   Widget _buildAddTaskButton() {
     return FloatingActionButton(
       child: const Icon(Icons.add),
@@ -34,31 +43,31 @@ class _PlanScreenState extends State<PlanScreen> {
       },
     );
   }
-  
+
   Widget _buildList() {
     return ListView.builder(
       itemCount: plan.tasks.length,
-      itemBuilder: (context, index) =>
-          _buildTaskTile(plan.tasks[index], index),
+      itemBuilder: (context, index) => _buildTaskTile(plan.tasks[index], index),
     );
   }
-  
+
   Widget _buildTaskTile(Task task, int index) {
     return ListTile(
       leading: Checkbox(
-          value: task.complete,
-          onChanged: (selected) {
-            setState(() {
-              plan = Plan(
-                name: plan.name,
-                tasks: List<Task>.from(plan.tasks)
-                  ..[index] = Task(
-                    description: task.description,
-                    complete: selected ?? false,
-                  ),
-              );
-            });
-          }),
+        value: task.complete,
+        onChanged: (selected) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: task.description,
+                  complete: selected ?? false,
+                ),
+            );
+          });
+        },
+      ),
       title: TextFormField(
         initialValue: task.description,
         onChanged: (text) {
@@ -66,10 +75,7 @@ class _PlanScreenState extends State<PlanScreen> {
             plan = Plan(
               name: plan.name,
               tasks: List<Task>.from(plan.tasks)
-                ..[index] = Task(
-                  description: text,
-                  complete: task.complete,
-                ),
+                ..[index] = Task(description: text, complete: task.complete),
             );
           });
         },
